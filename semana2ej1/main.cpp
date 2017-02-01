f71c8573176ffc99ad33caae9ab19894edacf3ab
cesartaboras <cesar_taboras@hotmail.com>
2017-02-01 07:26:35 -0600

    Add files via upload

---
+++ semana2ej1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int x;
+int main()
+{
+    /*x++;
+    cout<<x<<endl;
+    cout.flush();
+    main();*/
+    while(true)
+        new int(0);
+
+    return 0;
+}
